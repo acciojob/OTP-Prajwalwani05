badcd11559165fd6b3e5f39c59befe26000a3e82
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-28 12:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+
+      // move focus to the next input on typing a number
+      var inputs = document.querySelectorAll('.code');
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('keyup', function(e) {
+          if (this.value.length == this.maxLength) {
+            var index = Array.prototype.indexOf.call(inputs, this);
+            if (index !== inputs.length - 1) {
+              inputs[index + 1].focus();
+            }
+          }
+        });
+      }
+      
+      // move focus to the previous input on backspace key press
+      for (var i = inputs.length - 1; i >= 0; i--) {
+        inputs[i].addEventListener('keydown', function(e) {
+          if (e.keyCode == 8 && this.value.length == 0) {
+            var index = Array.prototype.indexOf.call(inputs, this);
+            if (index !== 0) {
+              inputs[index - 1].focus();
+            }
+          }
+        });
+      }
