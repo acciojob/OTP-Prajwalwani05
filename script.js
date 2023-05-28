ea6099ca5abd1464bddaf0455472c422cbf11eec
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-28 12:43:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,13 +13,13 @@
       }
       
       // move focus to the previous input on backspace key press
-for (var i = inputs.length - 1; i >= 0; i--) {
- inputs[i].addEventListener('keydown', function(e) {
+ for (var i = inputs.length - 1; i >= 0; i--) {
+  inputs[i].addEventListener('keydown', function(e) {
    if (e.keyCode == 8 && this.value.length == 0) {
-    var index = Array.prototype.indexOf.call(inputs, this);
-   if (index !== 0) {
-   inputs[index - 1].focus();
+     var index = Array.prototype.indexOf.call(inputs, this);
+     if (index !== 0) {
+     inputs[index - 1].focus();
+     }
    }
-    }
-    });
-    }
+ });
+}
